e564c8ae38b55d5630b7ff478bb19eb3c641fa43
Luis Fillipe Aires Souza <luisfillipe.lipe@gmail.com>
2025-08-10 02:54:17 -0300

    selector update, better error handling

--- bots/unfollow.js
+++ bots/unfollow.js
@@ -15,7 +15,7 @@ const selectors = {
   window: 'div[class="x6nl9eh x1a5l9x9 x7vuprf x1mg3h75 x1lliihq x1iyjqo2 xs83m0k xz65tgg x1rife3k x1n2onr6"]',
   profiles: 'div[class="html-div xdj266r x14z9mp xat24cr x1lziwak xexx8yu xyri2b x18d9i69 x1c1uobl x9f619 xjbqb8w x78zum5 x15mokao x1ga7v0g x16uus16 xbiv7yw x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1"]',
   openOptionsButton: 'div[class="_ap3a _aaco _aacw _aad6 _aade"]',
-  windowButtons: 'div[class="x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xc5r6h4 xqeqjp1 x1phubyo x13fuv20 x18b5jzi x1q0q8m5 x1t7ytsu x972fbf x10w94by x1qhh985 x14e42zd x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x14z9mp xat24cr x1lziwak x2lwn1j xeuugli xexx8yu xyri2b x18d9i69 x1c1uobl x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x87ps6o x1lku1pv x1a2a7pz x1qnrgzn x1cek8b2 xb10e19 x19rwo8q x1lliihq x193iq5w xh8yej3"]',
+  windowButtons: 'div[class="x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xc5r6h4 xqeqjp1 x1phubyo x13fuv20 x18b5jzi x1q0q8m5 x1t7ytsu x972fbf x10w94by x1qhh985 x14e42zd x9f619 x1ypdohk xdl72j9 x2lah0s x3ct3a4 xdj266r x14z9mp xat24cr x1lziwak x2lwn1j xeuugli xexx8yu xyri2b x18d9i69 x1c1uobl x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x87ps6o x1lku1pv x1a2a7pz x1qnrgzn x1cek8b2 xb10e19 x19rwo8q x1lliihq x193iq5w xh8yej3"]',
   confirmButton: 'button[class="_a9-- _ap36 _a9-_"]',
 }
 const page = await loadLoggedInPage()
@@ -85,6 +85,12 @@ while (continueScript) {
   } catch (e) {
     console.log(new Date().toLocaleTimeString(), 'ERROR: Something went wrong, restarting script')
     console.error(e)
+
+    let pages = await browser.pages()
+    while (pages.length > 1) {
+      await pages[pages.length - 1].close()
+      pages = await browser.pages()
+    }
   }
 }
 console.log('Unfollowed count:', unfollowedCount)
